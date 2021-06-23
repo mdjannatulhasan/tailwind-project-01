3cb52d306ba94ef68ae1344ee02a614dd9c676ef
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2021-06-23 11:42:25 +0600

    1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 module.exports = {
   purge: [
     './public/**/*.html',
-    './public/*.html',
      './public/**/*.js',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    container: {
+      center: true,
+      padding: '24px'
+    },
+    extend: {
+      colors: {
+        primary: '#FF6600',
+        body: '#4A557C',
+        heading: '#000B33'
+      }
+    },
   },
   variants: {
     extend: {},
